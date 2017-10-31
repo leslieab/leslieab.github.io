0c5a309d45c2690b29fd38fe6d0287e431751d97
leslieab <leslie.bloomfield@ni.com>
2017-10-31 11:08:35 -0500

    Update string.js

--- string.js
+++ string.js
@@ -1,81 +1,81 @@
 /* STRING */ 
 
+(function() {
+  import {label} from 'https://leslieab.github.io/label.js';
 
-class NIString extends HTMLElement {
-  constructor() {
-    super();
+  class NIString extends HTMLElement {
+    constructor() {
+      super();
     
-    const template = document
-    .createElement('template');
-    
-    template.innerHTML = `
-  <div class='control'>
-    <div class='container input'>
-      <textarea class='field'></textarea>
-    </div>
-  </div>
-`;
-    
-    this.userType = this.userType.bind(this);
-    this.append(template.content.cloneNode(true));
+      const template = document.createElement('template');
+      
+      template.innerHTML = `
+        <div class='control'>
+          <div class='container input'>
+            <textarea class='field'></textarea>
+          </div>
+        </div>
+      `;
+      
+      this.userType = this.userType.bind(this);
+      this.append(template.content.cloneNode(true));
 
-    this.container = this.querySelector('.container');
-    this.field = this.querySelector('.field');
-    this.control = this.querySelector('.control');
-    
-    import {label} from 'https://leslieab.github.io/label.js';
-    
-    if (this.hasAttribute("label")) {
-      const label = customElements.get('ni-label');
-      var newLabel = new label();
-      this.insertBefore(newLabel, this.control);
+      this.container = this.querySelector('.container');
+      this.field = this.querySelector('.field');
+      this.control = this.querySelector('.control');
+      
+      if (this.hasAttribute("label")) {
+        const label = customElements.get('ni-label');
+        var newLabel = new label();
+        this.insertBefore(newLabel, this.control);
+      }
+      
+      this.label = this.querySelector('.label'); 
     }
+    connectedCallback() {
+      this.field.addEventListener('change', this.userType);
     
-    this.label = this.querySelector('.label'); 
-  }
-  connectedCallback() {
-    this.field
-      .addEventListener('change', this.userType);
-    
-    if (this.hasAttribute('wrap', 'off')) {
-      this.field.addEventListener('keydown', this.returnKey);
+      if (this.hasAttribute('wrap', 'off')) {
+        this.field.addEventListener('keydown', this.returnKey);
+      }
     }
-  }
-  userType() {
-     var changedText = this.field.value;
-     this.text = changedText;
-  }
-  returnKey(e) {
-    if (e.which === 13) {
-      e.preventDefault();
-    } 
-  }
-  static get observedAttributes() {
-    return ['value', 'placeholder', 'label'];
-  }
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'value') {
-      this.newText = newValue;
-      this.field.value = newValue;
-    };
-    
-    if (name === 'placeholder') {
-      this.field.setAttribute('placeholder', newValue);
-    };
-    
-    if (name === 'label') {
-      this.label.textContent = newValue;
-    };
-  }
-  get text() {
-    return this.newText;
-  }
-  set text(v) {
-    this.setAttribute('value', v);
-  }
-  disconnectedCallback() {
-    this.field.removeEventListener('change', this.userType);
-  }
-};
-
-customElements.define('ni-string', NIString);
+    userType() {
+      var changedText = this.field.value;
+      this.text = changedText;
+    }
+    returnKey(e) {
+      if (e.which === 13) {
+        e.preventDefault();
+      } 
+    }
+    static get observedAttributes() {
+      return ['value', 'placeholder', 'label'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === 'value') {
+        this.newText = newValue;
+        this.field.value = newValue;
+      };
+      
+      if (name === 'placeholder') {
+        this.field.setAttribute('placeholder', newValue);
+      };
+      
+      if (name === 'label') {
+        this.label.textContent = newValue;
+      };
+    }
+    get text() {
+      return this.newText;
+    }
+    set text(v) {
+      this.setAttribute('value', v);
+    }
+    disconnectedCallback() {
+      this.field.removeEventListener('change', this.userType);
+      this.field.removeEventListener('keydown', this.returnKey);
+    }
+  };
+  
+  customElements.define('ni-string', NIString);
+})();
