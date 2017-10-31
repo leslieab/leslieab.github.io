f9b37be2166a5d12ef41bcb498076c4d3ec17a40
leslieab <leslie.bloomfield@ni.com>
2017-10-31 11:12:54 -0500

    Update label.js

--- label.js
+++ label.js
@@ -1,16 +1,16 @@
 /* Label */
-class NILabel extends HTMLElement {
-  constructor() {
-    super();
-    
-    const template = document.createElement('template');
-    
-    template.innerHTML = `
-  <span class='label'>hi</span>
-`;
-    this.append(template.content.cloneNode(true));
-  }
-};
-
-customElements.define('ni-label', NILabel);
-export const label = customElements.get('ni-label');
+(function() {
+  class NILabel extends HTMLElement {
+    constructor() {
+      super();
+      
+      const template = document.createElement('template');
+      template.innerHTML = `
+        <span class='label'>hi</span>
+      `;
+      this.append(template.content.cloneNode(true));
+    }
+  };
+  customElements.define('ni-label', NILabel);
+  export const label = customElements.get('ni-label');
+})();
