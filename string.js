c044c4a4392f55e4c03b529045613adcb0223a73
leslieab <leslie.bloomfield@ni.com>
2017-10-31 16:39:54 -0500

    Update string.js

--- string.js
+++ string.js
@@ -9,7 +9,7 @@
       
       template.innerHTML = `
         <div class='control'>
-          <div class='container input'>
+          <div class='inner-container input'>
             <textarea class='field'></textarea>
           </div>
         </div>
@@ -18,7 +18,7 @@
       this.userType = this.userType.bind(this);
       this.append(template.content.cloneNode(true));
 
-      this.container = this.querySelector('.container');
+      this.container = this.querySelector('.inner-container');
       this.field = this.querySelector('.field');
       this.control = this.querySelector('.control');
       
